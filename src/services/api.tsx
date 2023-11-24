5a0875544d4f84e967c6f27967475ada1c7b4a51
Daniel Kriade <daniel_kraide@hotmail.com>
2023-11-24 20:10:02 -0300

    test new link api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined) {
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-         baseURL: 'https://http://api.krdsystem.com/api',
+         baseURL: 'https://api.krdsystem.com/api',
      //   baseURL: 'https://localhost:3334/api',
         headers: {
             Authorization: `Bearer ${cookies['@web_front.token']}`,
